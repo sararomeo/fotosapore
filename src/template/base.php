42802862171f45de50db8ab2dfb7e48e5214139f
Sara Romeo <sara.romeo3@studio.unibo.it>
2022-12-28 16:51:17 +0100

    Add icon to title bar

--- src/template/base.php
+++ src/template/base.php
@@ -6,7 +6,9 @@
 
     <link href=https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link rel="stylesheet" type="text/css" href="./css/style.css" />
-   
+
+    <!-- default title bar icon -->
+    <link href="res/favicon.png" rel="shortcut icon" type="image/x-icon" />
     <title><?php echo $templateParams["titolo"]; ?></title>
 </head>
 
