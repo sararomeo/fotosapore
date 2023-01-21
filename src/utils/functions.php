13e8a0d9047aa18bf2f2bd388188870769004e0c
Luca Babboni <93348024+ElektroDuck@users.noreply.github.com>
2023-01-21 16:10:52 +0100

    added sessions function

--- src/utils/functions.php
+++ src/utils/functions.php
@@ -7,8 +7,28 @@ require 'email_notification\sendEmail.php';
  */
 function sendRegistrationEmail($to){ 
     $subject = "Success registration to FotoSapore website";
-    $message = "You have successfully registered to FotoSapore website. Now you can login to your account and start using our services.";
+    $message = "You have successfully registered to FotoSapore website. Now you can login to your account and start using our services!!!";
     return EmailSender::sendEmail($to , $subject, $message );
 }
+
+/**
+ * Add a new session for the user
+ * 
+ * @param  $username the username of the user
+ * @param  $email the email of the user
+ */
+function openUserSession($username, $email){
+    $_SESSION["username"] = $username;
+    $_SESSION["mail"] = $email;
+}
+
+/**
+ * Check if is currently opened a session 
+ * 
+ */
+function isSessionOpen(){
+    return !empty($_SESSION['username']);
+}
+
 
 ?>
