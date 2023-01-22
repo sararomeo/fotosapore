71207b0741531e9af5a13205be7531228986d37e
Luca Babboni <93348024+ElektroDuck@users.noreply.github.com>
2023-01-22 21:44:37 +0100

    modified template and file accepted by the input

--- src/template/post-form.php
+++ src/template/post-form.php
@@ -1,5 +1,5 @@
 <form action="insert-article.php" method="POST" enctype="multipart/form-data">
-    
+
     <div class="row">
         <div class="col-md-5">
             <h2>Insert your new recipe</h2>
@@ -8,18 +8,23 @@
                 <label for="title">Title:</label><input type="text" class="form-control" id="title" name="title" />
             </div>
             <div class="form-outline p-2">
-                <label for="caption">Caption:</label><textarea id="caption" class="form-control "
-                    name="caption" rows="6"> </textarea>
+                <label for="caption">Caption:</label><textarea id="caption" class="form-control " name="caption"
+                    rows="6"> </textarea>
             </div>
             <div class="form-outline p-2">
-                <label for="recipe">Recipe:</label><textarea id="recipe" class="form-control" name="recipe" rows="6"></textarea>
+                <label for="recipe">Recipe:</label><textarea id="recipe" class="form-control" name="recipe"
+                    rows="6"></textarea>
             </div>
         </div>
 
         <div class="col-md-7">
+
             <div class="form-outline p-2">
-                <label for="imgarticle">Image</label><input type="file" name="imgarticle" id="imgarticle" />
+                <label for="imgarticle">Image</label><input type="file" name="imgarticle" id="imgarticle"
+                    accept="image/jpeg, image/png" />
             </div>
+
+
         </div>
     </div>
 
