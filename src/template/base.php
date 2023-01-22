61e9545b4e025842f1d29201f1c6ffbd03bed27a
Sara Romeo <sara.romeo3@studio.unibo.it>
2023-01-22 14:15:17 +0100

    Update base.php

--- src/template/base.php
+++ src/template/base.php
@@ -4,18 +4,13 @@
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-    <!-- Bootstrap CSS -->
+    <!-- Bootstrap, icon, font-awesome -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous" />
-    <style><?php include './css/style.css'; ?></style>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@6.2.1/css/fontawesome.min.css" integrity="sha384-QYIZto+st3yW+o8+5OHfT6S482Zsvz2WfOzpFSXMF9zqeLcFV0/wlZpMtyFcZALm" crossorigin="anonymous">
-    <!-- non so perché non funziona con html
-        <link rel="stylesheet" type="text/css" href="css/style-login.css" /> -->
-
-    
-
-    <!-- Default title bar icon -->
+    <!-- CSS -->
+    <link rel="stylesheet" type="text/css" href="./css/style.css" />
+    <!-- Default title bar with icon -->
     <link rel="shortcut icon" href="res/favicon.ico" type="image/x-icon" />
     <title><?php echo $templateParams["title"]; ?></title>
 </head>
