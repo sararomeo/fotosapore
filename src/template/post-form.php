4235c59db37743c80702cefe7154af982806bec2
Luca Babboni <93348024+ElektroDuck@users.noreply.github.com>
2023-01-22 21:35:56 +0100

    fixed design

--- src/template/post-form.php
+++ src/template/post-form.php
@@ -1,23 +1,33 @@
 <form action="insert-article.php" method="POST" enctype="multipart/form-data">
-    <h2>Insert your new recipe</h2>
-    <div class="mb-4 mt-4">
-        <!-- Text input -->
-        <div class="form-outline p-2">
-            <label for="title">Title:</label><input type="text" class="form-control" id="title" name="title" />
-        </div>
-        <div class="form-outline p-2">
-            <label for="caption">Caption:</label><textarea id="caption" class="form-control" name="caption"></textarea>
-        </div>
-        <div class="form-outline p-2">
-            <label for="recipe">Recipe:</label><textarea id="recipe" class="form-control" name="recipe"></textarea>
-        </div>
-        <div class="form-outline p-2">
-            <label for="imgarticle">Immagine Articolo</label><input type="file" name="imgarticle" id="imgarticle" />
+    
+    <div class="row">
+        <div class="col-md-5">
+            <h2>Insert your new recipe</h2>
+            <!-- Text input -->
+            <div class="form-outline p-2">
+                <label for="title">Title:</label><input type="text" class="form-control" id="title" name="title" />
+            </div>
+            <div class="form-outline p-2">
+                <label for="caption">Caption:</label><textarea id="caption" class="form-control "
+                    name="caption" rows="6"> </textarea>
+            </div>
+            <div class="form-outline p-2">
+                <label for="recipe">Recipe:</label><textarea id="recipe" class="form-control" name="recipe" rows="6"></textarea>
+            </div>
         </div>
 
-        <!-- Submit buttons -->
-        <div class="d-flex justify-content-end">
-            <input type="submit" name="submit"/>
-            <a href="home.php">Annulla</a> <!-- modify link -->
+        <div class="col-md-7">
+            <div class="form-outline p-2">
+                <label for="imgarticle">Image</label><input type="file" name="imgarticle" id="imgarticle" />
+            </div>
         </div>
+    </div>
+
+    <!-- Submit buttons -->
+    <div class="row mt-5">
+        <div class="d-flex justify-content-start ">
+            <button type="submit" class="btn btn-primary">Publish post</button>
+        </div>
+    </div>
+
 </form>
--- src/template/post-tmpl.php
+++ src/template/post-tmpl.php
@@ -1,17 +1,8 @@
 <div class="container-fluid">
-    <div class="row w-100 vh-100 form-page">
-        <!-- upload imgs on the left -->
-        <div class="col-md p-4 d-flex flex-column align-items-left justify-content-center form-text-area">
-            <div class="d-flex mt-0">
-                <h1 class="h1 display-1 fw-bold p-4">Create post</h1>
-            </div>
-            <div class="p-4 d-flex fs-3 m-2">
-                <p class="w-100">Upload your picture:</p>
-            </div>
-        </div>
+    <div class="col-md p-4 d-flex flex-column align-items-left justify-content-center form-text-area">
         <!-- text form on the right -->
-        <div class="col-md p-4 d-flex flex-column align-items-center justify-content-center">
-             <?php require("post-form.php"); ?> 
-        </div>
+        <?php require("post-form.php"); ?> 
+        <!-- <div class="col-md p-4 d-flex flex-column align-items-center justify-content-center">
+        </div> -->
     </div>
 </div>
