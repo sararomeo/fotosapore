9e8b026ea157c22eedb11bccb76cbc81c949630e
Luca Babboni <93348024+ElektroDuck@users.noreply.github.com>
2023-01-22 18:46:42 +0100

    added database dumb with updated tables

--- doc/fotosaporeWhithoutCreation.sql
+++ doc/fotosaporeWhithoutCreation.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Creato il: Gen 22, 2023 alle 18:40
+-- Creato il: Gen 22, 2023 alle 18:41
 -- Versione del server: 10.4.27-MariaDB
 -- Versione PHP: 8.2.0
 
