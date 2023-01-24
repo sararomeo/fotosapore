a20e5a9fa7452547d35783c78eee8362609093da
Pablo Vargas <85257320+Zettaiki@users.noreply.github.com>
2023-01-24 01:29:41 +0100

    Add search.php

---
+++ src/search.php
@@ -0,0 +1,8 @@
+<?php
+require_once("bootstrap.php");
+
+$templateParams["title"] = "Foto Sapore | Search";
+$templateParams["page"] = "search-tmpl.php";
+
+require("template/base.php");
+?>
---
+++ src/template/search-tmpl.php
@@ -0,0 +1,7 @@
+<div id="page-name" data-page="search-page">
+    <div class="d-flex justify-content-center">
+        <div class = "scroll-container" id = "scroll-container">
+            
+        </div>
+    </div>
+</div>
