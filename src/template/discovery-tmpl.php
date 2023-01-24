0c304b5519ddfb262a306fd787964fc18de26a12
Pablo Vargas <85257320+Zettaiki@users.noreply.github.com>
2023-01-24 01:29:59 +0100

    Update template

--- src/template/discovery-tmpl.php
+++ src/template/discovery-tmpl.php
@@ -1,8 +1,7 @@
 <div id="page-name" data-page="discovery-page">
-
-<div class="d-flex justify-content-center">
-<div class = "scroll-container" id = "scroll-container">
-    
-</div>
-
+    <div class="d-flex justify-content-center">
+        <div class = "scroll-container" id = "scroll-container">
+            
+        </div>
+    </div>
 </div>
--- src/template/profile-tmpl.php
+++ src/template/profile-tmpl.php
@@ -1,8 +1,7 @@
 <div id="page-name" data-page="profile-page">
-
-<div class="d-flex justify-content-center">
-<div class = "feed">
-    PROFILE OWNER FEED POSTS HERE
-</div>
-
+    <div class="d-flex justify-content-center">
+        <div class = "scroll-container" id = "scroll-container">
+            
+        </div>
+    </div>
 </div>
