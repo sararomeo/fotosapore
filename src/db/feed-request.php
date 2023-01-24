7f3c17b16bb02d24ae7881cbb23f8b9229b584b7
Pablo Vargas <85257320+Zettaiki@users.noreply.github.com>
2023-01-24 02:15:14 +0100

    Update feed-request.php

--- src/db/feed-request.php
+++ src/db/feed-request.php
@@ -1,13 +1,20 @@
 <?php
     require_once("database.php");
     require_once("../bootstrap.php");
+    
     $args = json_decode($_POST["args"], false);
     
     if($args->pageName == "home.php") {
-        $postArray = $dbh->getFeedPosts();
+        $postArray = $dbh->getHomePosts();
         $postNum = count($postArray);
     } else if($args->pageName == "discovery.php") {
         $postArray = $dbh->getDiscoveryPosts();
+        $postNum = count($postArray);
+    } else if($args->pageName == "profile.php") {
+        $postArray = $dbh->getProfilePosts();
+        $postNum = count($postArray);
+    } else if($args->pageName == "search.php") {
+        $postArray = $dbh->getSearchPosts();
         $postNum = count($postArray);
     }
 
