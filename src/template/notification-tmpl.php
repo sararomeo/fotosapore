06a3ec9ee9cbe7af7902a241f50215c4940e675f
Luca Babboni <93348024+ElektroDuck@users.noreply.github.com>
2023-01-24 14:52:57 +0100

    added data

--- src/template/notification-tmpl.php
+++ src/template/notification-tmpl.php
@@ -5,19 +5,18 @@
         </div>
     </div>
 
-
     <div class="row d-flex justify-content-center">
         <div class="col-md-8 col-lg-6">
             <div class="card shadow-0 border" style="background-color: #f0f2f5;">
                 <div class="card-body p-4">
                     <?php foreach ($templateParams["notifications"] as $n): ?>
-                        <div class="card mb-4">
+                        <div class="card my-4">
                             <div class="card-body mx-4 my-4">
                                 <h2 class="display-6">
                                     <?php echo $n["text"] ?>
                                 </h2>
                                 <p>
-                                    <?php echo $n["text"] ?>
+                                    <?php echo date($n["time"]) ?>
                                 </p>
                             </div>
                         </div>
