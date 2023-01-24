f81c1c9a171ef27be6d42d0b412a62d0f10b4004
Luca Babboni <93348024+ElektroDuck@users.noreply.github.com>
2023-01-24 17:11:18 +0100

    added page for modify user profile

--- src/template/edit-profile-tmpl.php
+++ src/template/edit-profile-tmpl.php
@@ -1,22 +1,38 @@
 <div id="page-name" data-page="profile-page">
     <div class="profile-feed-container d-flex flex-column aligns-items-center">
-        <div class="feed align-self-center">
-            EDIT PROFILE HERE TO DO
-        </div>
         <div class="profile-info align-self-center p-2">
             <?php
+            //need to change userIDtest to the actual userID
             $userIDtest = 1;
             $profileData = $dbh->getUserProfileInfo($userIDtest);
-            $followers = $dbh->getFollowersCount($userIDtest)['followers'];
-            $following = $dbh->getFollowingsCount($userIDtest)['following'];
             ?>
-            <div class="d-flex justify-content-center">
-                <h1 class="h1 p-1 text-center"><?php echo $profileData['username']; ?></h1>
-            </div>
-            <div class="d-flex justify-content-center">
-                <h2 class="h2 p-1 text-center"><?php echo $profileData['bio']; ?></h2>
-            </div>
-        </div>
 
+            <div class="d-flex justify-content-center">
+                <div class="row">
+                    <h1 class="h2 p-2 col-12">Edit your profile informations:</h1>
+                </div>
+            </div>
+
+            <form action="elaborate-edit-profile.php" method="POST" enctype="multipart/form-data">
+
+                <!-- user informations --> 
+                <div class="form-outline p-2">
+                    <label for="username">Username:</label><input type="text" class="form-control" id="username"
+                        name="username" required value="<?php echo ($profileData["username"]); ?>" />
+                </div>
+
+                <div class="form-outline p-2">
+                    <label for="bio">Bio:</label>
+                    <textarea id="bio" class="form-control " name="bio" required rows="6"><?php echo ($profileData["bio"]);?></textarea>
+                </div>
+
+                <!-- Submit buttons -->
+                <div class="d-flex justify-content-center py-4">
+                    <input class="form-btn align-right p-2 m-2 rounded" type="submit" value="Update profile" />
+                </div>
+
+
+            </form>
+        </div>
     </div>
 </div>
