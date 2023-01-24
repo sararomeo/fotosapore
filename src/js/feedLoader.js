1c81ea698ed4c210a9f78612b08ae567b48aa402
Pablo Vargas <85257320+Zettaiki@users.noreply.github.com>
2023-01-24 01:19:23 +0100

    Update feedLoader.js

--- src/js/feedLoader.js
+++ src/js/feedLoader.js
@@ -1,15 +1,8 @@
 // Variables
-const container = document.getElementById('container');
-var index = 0;
-var maxIndex;
-
-// Main
-getPost();
-getPost();
-getPost();
-getPost();
-getPost();
-getPost();
+const container = document.getElementById('scroll-container');
+const pageName = location.href.substring(location.href.lastIndexOf('/')+1);
+var getPostIndex = 0;
+var addDataIndex = 0;
 
 /**
  * Event listener for the scrolling, if the user reach the end of the page.
@@ -27,9 +20,7 @@ window.addEventListener('scroll', () => {
  * Load the next post in the index.
  */
 function getPost() {
-    if(index >= maxIndex) return;
-
-    let args = {"value":index};
+    let args = {"value":getPostIndex, "pageName":pageName};
     let jsonArgs = JSON.stringify(args);
 
     var xmlhttp = new XMLHttpRequest();
@@ -41,7 +32,7 @@ function getPost() {
     xmlhttp.open("POST", "db/feed-request.php", true);
     xmlhttp.setRequestHeader("content-type","application/x-www-form-urlencoded");
     xmlhttp.send("args=" + jsonArgs);
-    index = index + 1;
+    getPostIndex = getPostIndex + 1;
 }
 
 /**
@@ -49,14 +40,33 @@ function getPost() {
  * @param {*} response The JSON to be loaded in the feed.
  */
 function addDataToFeed(response) {
-    maxIndex = response.postNum;
-    postContent = response.postArray;
+    if(response.postNum <= addDataIndex || response.postArray == null) return;
     const postElement = document.createElement('div');
     postElement.classList.add('user-post');
-    postElement.innerHTML = `
-		<h1 class="title">${postContent.title}</h2>
-		<p class="text">${postContent.caption}</p>
-        <p class="text2">${postContent.timestamp}</p>
-	`;
-    container.appendChild(postElement);
+
+    const div = Object.assign(document.createElement("div"),{className:"user-post"});
+    const h1 = Object.assign(document.createElement("h1"),{className:"title",innerText:response.postArray.title});
+    const p1 = Object.assign(document.createElement("p"),{className:"text1",innerText:response.postArray.timestamp});
+    const p2 = Object.assign(document.createElement("p"),{className:"text2",innerText:response.postArray.caption});
+    const p3 = Object.assign(document.createElement("p"),{className:"text3",innerText:response.postArray.recipe});
+    const h2 = Object.assign(document.createElement("h2"),{className:"title2",innerText:response.postArray.username});
+
+
+    div.appendChild(h1);
+    div.appendChild(p1);
+    div.appendChild(p2);    
+    div.appendChild(p3);
+    div.appendChild(h2);
+
+
+    container.appendChild(div);
+    addDataIndex = addDataIndex + 1;
 }
+
+// Main
+getPost();
+getPost();
+getPost();
+getPost();
+getPost();
+getPost();
