59e5ff9b182855769a8f013497fe280b9a9feed7
Luca Babboni <93348024+ElektroDuck@users.noreply.github.com>
2023-01-25 12:13:37 +0100

    created template for the singe post page

--- src/template/single-post-tmpl.php
+++ src/template/single-post-tmpl.php
@@ -1,6 +1,48 @@
-<div class="container-fluid">
-    <div class="row">
-        <h1></h1>
-    </div>
+<!-- post container -->
+<div class="container-fluid shadow border border-secondary">
 
+    <article>
+        <!-- article title --> 
+        <header>
+            <div class="row">
+                <div class="col-md-2 col-1"></div>
+                <div class="col-md-8 col-10">
+                    <h1 class="h1 text-center">
+                        <?php echo ($templateParams["postInfo"]["title"]) ?>
+                    </h1>
+                </div>
+                <div class="col-md-2 col-1"></div>
+            </div>
+        </header>
+
+        <section>
+            <div class="row">
+                <div class="col-md-5 col-sm-12 d-flex justify-content-center">
+                    <img src="<?php echo UPLOAD_DIR . $templateParams["postInfo"]["imagePath"]; ?>" class="img-thumbnail"
+                        alt="recipe result image" />
+                </div>
+        
+                <div class="col-md-1"></div>
+        
+                <div class="col-md-6 col-sm-12">
+                    <h2>Caption:</h2>
+                    <p><?php echo $templateParams["postInfo"]["caption"];?></p>
+                    
+                    <h2>Recipe:</h2>
+                    <p><?php echo $templateParams["postInfo"]["recipe"];?></p>
+
+                    <h2>Tags:</h2>
+                    <?php foreach($templateParams["postInfo"]["tagString"] as $tag):?>
+                        <p><?php echo $tag["tag"];?></p>
+                        <a href="search.php?myInput=<?php echo $tag["tag"]; ?>"><?php echo $tag["tag"];?></a>
+                    <?php endforeach;?>
+
+                </div>
+        </section>
+    </article>
+
+
+
+
+    </div>
 </div>
