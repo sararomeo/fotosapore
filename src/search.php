dfecc788fb1da6e10a48e507fa0ca5fc79037bb0
Luca Babboni <93348024+ElektroDuck@users.noreply.github.com>
2023-01-25 12:37:28 +0100

    added post input

--- src/search.php
+++ src/search.php
@@ -1,6 +1,18 @@
 <?php
 require_once("bootstrap.php");
 
+if(!isSessionOpen()){
+    header("location: index.php");
+}
+
+if (isset($_POST['myInput'])){ 
+    $tagsString = $_POST['myInput'];
+}else if(isset($_GET['myInput'])){ 
+    $tagsString = $_GET['myInput'];
+}else{ 
+    header("location: index.php");
+}
+
 $templateParams["title"] = "Foto Sapore | Search";
 $templateParams["page"] = "search-tmpl.php";
 
--- src/template/search-tmpl.php
+++ src/template/search-tmpl.php
@@ -1,16 +1,6 @@
 <div id="page-name" data-page="search-page">
     <div class="d-flex justify-content-center">
         <div class = "">
-            <?php
-            if (!isset($_POST['myInput'])) {
-                //header("location: ../index.php");
-            } else {
-                $tagsString = $_POST['myInput'];
-                if($tagsString == "") {
-                    //header("location: ../index.php");
-                }
-            }
-            ?>
             <?php if ($dbh->getSearchPosts($tagsString) == null): ?>
                 <p class='text-center'>No post with such tag was found.</p>
             <?php endif;?>
