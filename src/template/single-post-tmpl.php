98a95d9e44bc331bac81d7092361be21cc375f89
Sara Romeo <sara.romeo3@studio.unibo.it>
2023-01-25 14:34:19 +0100

    Inline tags single post

--- src/template/single-post-tmpl.php
+++ src/template/single-post-tmpl.php
@@ -2,7 +2,7 @@
 <div class="container-fluid shadow border border-secondary">
 
     <article>
-        <!-- article title --> 
+        <!-- article title -->
         <header>
             <div class="row">
                 <div class="col-md-2 col-1"></div>
@@ -18,26 +18,39 @@
         <section>
             <div class="row">
                 <div class="col-md-5 col-sm-12 d-flex justify-content-center">
-                    <img src="<?php echo UPLOAD_DIR . $templateParams["postInfo"]["imagePath"]; ?>" class="img-thumbnail"
-                        alt="recipe result image" />
+                    <img src="<?php echo UPLOAD_DIR . $templateParams["postInfo"]["imagePath"]; ?>"
+                        class="img-thumbnail" alt="recipe result image" />
                 </div>
-        
+
                 <div class="col-md-1"></div>
-        
+
                 <div class="col-md-6 col-sm-12">
                     <h2>Caption:</h2>
-                    <p><?php echo $templateParams["postInfo"]["caption"];?></p>
-                    
+                    <p>
+                        <?php echo $templateParams["postInfo"]["caption"]; ?>
+                    </p>
+
                     <h2>Recipe:</h2>
-                    <p><?php echo $templateParams["postInfo"]["recipe"];?></p>
+                    <p>
+                        <?php echo $templateParams["postInfo"]["recipe"]; ?>
+                    </p>
 
                     <h2>Tags:</h2>
-                    <?php foreach($templateParams["postInfo"]["tagString"] as $tag):?>
+                    <?php foreach ($templateParams["postInfo"]["tagString"] as $tag): ?>
                         <!-- <a href="search.php?myInput=<?php //echo $tag["tag"]; ?>"><?php //echo $tag["tag"];?></a> -->
-                        <form action="search.php?myInput=<?php echo $tag["tag"]; ?>" class="search-box" method="POST">
-                        <button type="submit" class="btn link-danger"><?php echo $tag["tag"];?></button>
-                    </form>
-                    <?php endforeach;?>
+                        <div class="float-start">
+                            <ul class="list-inline">
+                                <li class="list-inline-item">
+                                    <form action="search.php?myInput=<?php echo $tag["tag"]; ?>" class="search-box"
+                                        method="POST">
+                                        <button type="submit" class="btn link-danger">
+                                            <?php echo $tag["tag"]; ?>
+                                        </button>
+                                    </form>
+                                </li>
+                            </ul>
+                        </div>
+                    <?php endforeach; ?>
 
                 </div>
         </section>
@@ -46,5 +59,5 @@
 
 
 
-    </div>
+</div>
 </div>
