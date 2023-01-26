2f9d9962946a44aa72deb5e790e1b95261aa522e
Pablo Vargas <85257320+Zettaiki@users.noreply.github.com>
2023-01-26 01:01:10 +0100

    Update likeBtn.js

--- src/js/likeBtn.js
+++ src/js/likeBtn.js
@@ -1,26 +1,28 @@
 import ajaxRequest from './request.js';
 
-function toggleLike(likeValue, target) {
+function toggleLike(el, postID) {
+    var operation;
+    if(el.value == "like") {
+        operation = 1;
+    } else {
+        operation = 0;
+    }
+    
     const args={
-        like: target,
-        likeValue: likeValue
+        postID: postID,
+        op: operation
     };
 
-    ajaxRequest('./db/follow-request.php', function () {
-        $("#profile-btn").val(followValue);
+    ajaxRequest('./db/like-request.php', function () {
+        if(el.value == "like") {
+            el.value = "nolike";
+        } else {
+            el.value = "like";
+        }
+        el.classList.toggle("bi-heart");
+        el.classList.toggle("bi-heart-fill");
+        el.classList.toggle("text-danger");
     }, args);
 }
 
-$("#like-btn").click(function () {
-    console.log($(this).attr('value'));
-    if ($(this).attr('value') == "like") {
-        //toggleFollow("unlike", $(this).attr('data-target'));
-
-    } else {
-        //toggleFollow("like", $(this).attr('data-target'));
-        
-    }
-    this.classList.toggle("bi-heart");
-    this.classList.toggle("bi-heart-fill");
-    this.classList.toggle("text-danger")
-});
+export default toggleLike;
