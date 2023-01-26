17205496a28d1fc8cd9b8628d3757796cfb46a1c
Pablo Vargas <85257320+Zettaiki@users.noreply.github.com>
2023-01-26 18:52:50 +0100

    Update format

--- src/utils/notify-system.php
+++ src/utils/notify-system.php
@@ -3,7 +3,8 @@
 /**
  * Send an email to the user who has just registered to the website.
  */
-function sendRegistrationEmail($to) {
+function sendRegistrationEmail($to)
+{
     $subject = "Successfull registration to FotoSapore website";
     $message = "You have successfully registered to FotoSapore website. Now you can login to your account and start using our services!";
     return EmailSender::sendEmail($to, $subject, $message);
@@ -12,36 +13,40 @@ function sendRegistrationEmail($to) {
 /**
  * Send an email that notify that a followed user published a post
  */
-function sendPostNotification($to, $username) {
+function sendPostNotification($to, $username)
+{
     $subject = "A followed user published a post";
-    $message = $username." published a new post.";
+    $message = $username . " published a new post.";
     return EmailSender::sendEmail($to, $subject, $message);
 }
 
 /**
  * Send an email that notify that a leave a comment on a Post
  */
-function sendCommentNotification($to, $username) {
+function sendCommentNotification($to, $username)
+{
     $subject = "A user commented your post";
-    $message = $username." commented your post.";
+    $message = $username . " commented your post.";
     return EmailSender::sendEmail($to, $subject, $message);
 }
 
 /**
  * Send an email that notify that a leave a comment on a Post
  */
-function sendLikeNotification($to, $username) {
+function sendLikeNotification($to, $username)
+{
     $subject = "A user liked your post";
-    $message = $username." liked your post.";
+    $message = $username . " liked your post.";
     return EmailSender::sendEmail($to, $subject, $message);
 }
 
 /**
  * Send an email that notify that a user start following 
  */
-function sendFollowNotification($to, $username) {
+function sendFollowNotification($to, $username)
+{
     $subject = "A user started following you";
-    $message = $username." started following your profile.";
+    $message = $username . " started following your profile.";
     return EmailSender::sendEmail($to, $subject, $message);
 }
 
