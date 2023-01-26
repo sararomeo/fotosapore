60935051f80e833fe327ead77ede01a7c5ba440e
Sara Romeo <sara.romeo3@studio.unibo.it>
2023-01-26 17:55:23 +0100

    Caps h1

--- src/template/signup-tmpl.php
+++ src/template/signup-tmpl.php
@@ -4,7 +4,7 @@
         <div class="col-md p-4 d-flex flex-column align-items-center justify-content-center form-text-area">
             <div class="d-flex mt-0 align-items-center">
                 <img class="img-fluid float-left logo" src="res/big-favicon.png" alt="Tomato logo">
-                <h1 class="h1 display-1 fw-bold p-4">Foto sapore</h1>
+                <h1 class="h1 display-1 fw-bold p-4">Foto Sapore</h1>
             </div>
             <div class="d-flex fs-3 m-2">
                 <p class="text-center w-100">Insert your data to sign up and start exploring new dishes!</p>
