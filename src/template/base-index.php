69bc54b4876268db67fc25c50257e28dd596bee7
Sara Romeo <sara.romeo3@studio.unibo.it>
2023-01-26 18:45:01 +0100

    Include css

--- src/template/base-index.php
+++ src/template/base-index.php
@@ -9,9 +9,7 @@
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
         integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous" />
-    <style>
-        <?php include './css/style.css'; ?>
-    </style>
+    <link rel="stylesheet" type="text/css" href="./css/style.css" />
 
     <!-- Default title bar icon -->
     <link rel="shortcut icon" href="res/favicon.ico" type="image/x-icon" />
