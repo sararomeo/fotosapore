18fac6c83cd424e7f6b96b4ae6ed5776c921bfb2
Pablo Vargas <85257320+Zettaiki@users.noreply.github.com>
2023-01-26 18:42:09 +0100

    Update base-index.php

--- src/template/base-index.php
+++ src/template/base-index.php
@@ -1,26 +1,30 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous" />
-    <style><?php include './css/style.css'; ?></style>
-    <!-- non so perché non funziona con html
-        <link rel="stylesheet" type="text/css" href="css/style-login.css" /> -->
-
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
+        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous" />
+    <!-- <style><?php //include './css/style.css'; ?></style> -->
+    <link rel="stylesheet" type="text/css" href="css/style-login.css" />
+    
     <!-- Default title bar icon -->
     <link rel="shortcut icon" href="res/favicon.ico" type="image/x-icon" />
-    <title><?php echo $templateParams["title"]; ?></title>
+    <title>
+        <?php echo $templateParams["title"]; ?>
+    </title>
 </head>
 
 <body>
     <?php
-    if(isset($templateParams["page"])){
+    if (isset($templateParams["page"])) {
         require_once($templateParams["page"]);
     }
     ?>
 </body>
+
 </html>
