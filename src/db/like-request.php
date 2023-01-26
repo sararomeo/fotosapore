f2c78b649d1cb6adc3f9056df98c87caa5ebed6d
Pablo Vargas <85257320+Zettaiki@users.noreply.github.com>
2023-01-26 12:20:05 +0100

    Update like-request.php

--- src/db/like-request.php
+++ src/db/like-request.php
@@ -2,9 +2,9 @@
     require_once("../bootstrap.php");
 
     $args = json_decode($_POST['args'], false);
-    $args->postID;
-    $args->op;
+
     if ($args->op == 0) {
+        $dbh->insertLikeNotifications($_SESSION['userID'], $args->postID);
         $dbh->likePost($_SESSION['userID'], $args->postID);
     } else {
         $dbh->dislikePost($_SESSION['userID'], $args->postID);
