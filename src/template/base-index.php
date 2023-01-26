152f5e00fbdf7d4d2182f90b7536ce04a2ed00a2
Pablo Vargas <85257320+Zettaiki@users.noreply.github.com>
2023-01-26 18:44:00 +0100

    Update base-index.php

--- src/template/base-index.php
+++ src/template/base-index.php
@@ -9,9 +9,10 @@
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
         integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous" />
-    <!-- <style><?php //include './css/style.css'; ?></style> -->
-    <link rel="stylesheet" type="text/css" href="css/style-login.css" />
-    
+    <style>
+        <?php include './css/style.css'; ?>
+    </style>
+
     <!-- Default title bar icon -->
     <link rel="shortcut icon" href="res/favicon.ico" type="image/x-icon" />
     <title>
